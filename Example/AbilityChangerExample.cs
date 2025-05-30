b84eb5e8ce29b18fa7ce298ecd514e3ec68a9676
Pedro T <93625012+randomscorp@users.noreply.github.com>
2025-05-29 22:51:17 -0300

    Revert "Removes obsolete "Greeen Flower DG""
    This reverts commit 01cd85749e332405d4f9a07b76da962e6b6e11c1.

--- Example/AbilityChangerExample.cs
+++ Example/AbilityChangerExample.cs
@@ -13,9 +13,9 @@ namespace AbilityChangerExample
     public class RedFlowerDG : Dreamgate
     {
         static Sprite getActiveSprite() { return Satchel.AssemblyUtils.GetSpriteFromResources("red_flower.png"); }
-        public override string name { get => "Red Flower DG"; set { } }
+        public override string name { get => "Red Flower"; set { } }
 
-        public override string title { get => "Red Flower DG"; set { } }
+        public override string title { get => "Red Flower"; set { } }
         public override string description { get => "Plants a little red delicate flower"; set { } }
         public override Sprite activeSprite { get => getActiveSprite(); set { } }
 
@@ -38,6 +38,20 @@ namespace AbilityChangerExample
             };
         }
 
+    }
+    public class GreenFlowerDG : Dreamgate
+    {
+        static Sprite getActiveSprite() { return Satchel.AssemblyUtils.GetSpriteFromResources("green_flower.png"); }
+        public override string name { get => "Green Flower"; set { } }
+        public override string title { get => "Green Flower"; set { } }
+        public override string description { get => "Plants a little green delicate flower"; set { } }
+        public override Sprite activeSprite { get => getActiveSprite(); set { } }
+
+        public GreenFlowerDG()
+        {
+            ReplaceSpawn(AbilityChangerExample.green_flower);
+            relatedAbilities.Add(AbilityChanger.Abilities.DREAMNAIL, "Green Flower");
+        }
     }
 
     public class RedFlowerWings : DoubleJump
@@ -106,6 +120,9 @@ namespace AbilityChangerExample
             #endregion
 
             abilities = new() {
+                    new RedFlowerDG (),
+                    new GreenFlowerDG(),
+                    new RedFlowerWings(),
                     new Examples.CycloneSlashEx(),
                     new Examples.DashEx(),
                     new Examples.DashSlashEx(),
@@ -120,8 +137,6 @@ namespace AbilityChangerExample
                     new Examples.ScreamEx(),
                     new Examples.SuperDashEx(),
                     new Examples.WallJumpEx(),
-                    new RedFlowerDG (),
-                    new RedFlowerWings(),
                 };
 
             foreach (var ability in abilities)
